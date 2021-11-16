60d4521f082e39e233ed66cbd589b5d61038dbbe
HalilSarikas <91375385+HalilSarikas@users.noreply.github.com>
2021-11-16 20:16:07 +0300

    Add files via upload

---
+++ Kur Verisi Cekme Ve Hesaplama/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Kur_Verisi_Cekme_Ve_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDolar = new System.Windows.Forms.Label();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.lblSterlin = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.cboKur = new System.Windows.Forms.ComboBox();
+            this.txtKurAdet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(763, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Merkez Bankası Kur Verisi Çekme Ve Hesaplama";
+            // 
+            // lblDolar
+            // 
+            this.lblDolar.AutoSize = true;
+            this.lblDolar.Location = new System.Drawing.Point(43, 84);
+            this.lblDolar.Name = "lblDolar";
+            this.lblDolar.Size = new System.Drawing.Size(44, 13);
+            this.lblDolar.TabIndex = 0;
+            this.lblDolar.Text = "Dolar = ";
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Location = new System.Drawing.Point(43, 129);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(41, 13);
+            this.lblEuro.TabIndex = 0;
+            this.lblEuro.Text = "Euro = ";
+            // 
+            // lblSterlin
+            // 
+            this.lblSterlin.AutoSize = true;
+            this.lblSterlin.Location = new System.Drawing.Point(43, 174);
+            this.lblSterlin.Name = "lblSterlin";
+            this.lblSterlin.Size = new System.Drawing.Size(48, 13);
+            this.lblSterlin.TabIndex = 0;
+            this.lblSterlin.Text = "Sterlin = ";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(607, 238);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 1;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // cboKur
+            // 
+            this.cboKur.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboKur.FormattingEnabled = true;
+            this.cboKur.Location = new System.Drawing.Point(568, 95);
+            this.cboKur.Name = "cboKur";
+            this.cboKur.Size = new System.Drawing.Size(160, 21);
+            this.cboKur.TabIndex = 2;
+            // 
+            // txtKurAdet
+            // 
+            this.txtKurAdet.Location = new System.Drawing.Point(568, 122);
+            this.txtKurAdet.Name = "txtKurAdet";
+            this.txtKurAdet.Size = new System.Drawing.Size(160, 20);
+            this.txtKurAdet.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(565, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(511, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Kaç Adet";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(511, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Kur Cinsi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(26, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "5 Saniye\'de 1 Yeniler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 273);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKurAdet);
+            this.Controls.Add(this.cboKur);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblSterlin);
+            this.Controls.Add(this.lblEuro);
+            this.Controls.Add(this.lblDolar);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Merkez Bankası Kur Verisi Çekme Ve Hesaplama";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDolar;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.Label lblSterlin;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.ComboBox cboKur;
+        private System.Windows.Forms.TextBox txtKurAdet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Kur Verisi Cekme Ve Hesaplama/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Kur_Verisi_Cekme_Ve_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        double dolar, euro, sterlin;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void DovizGoster()
+        {
+            try
+            {
+                XmlDocument xmlVerisi = new XmlDocument();
+                xmlVerisi.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+
+                dolar = Convert.ToDouble(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "USD")).InnerText.Replace('.', ','));
+                euro = Convert.ToDouble(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "EUR")).InnerText.Replace('.', ','));
+                sterlin = Convert.ToDouble(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "GBP")).InnerText.Replace('.', ','));
+
+                lblDolar.Text = "Dolar = " + dolar.ToString("n2") + " TL";
+                lblEuro.Text = "Euro = " + euro.ToString("n2") + " TL";
+                lblSterlin.Text = "Sterlin = " + sterlin.ToString("n2") + " TL";
+            }
+            catch (XmlException xml)
+            {
+                timer1.Stop();
+                MessageBox.Show(xml.ToString());
+            }
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtKurAdet.Text))
+            {
+                var kursecim = cboKur.SelectedIndex;
+                var hesaplama = 0D;
+                switch (kursecim)
+                {
+                    case 0:
+                        hesaplama = dolar * (Convert.ToDouble(txtKurAdet.Text));
+                        break;
+                    case 1:
+                        hesaplama = euro * (Convert.ToDouble(txtKurAdet.Text));
+                        break;
+                    case 2:
+                        hesaplama = sterlin * (Convert.ToDouble(txtKurAdet.Text));
+                        break;
+                }
+                label2.Text = txtKurAdet.Text + " Adet " + cboKur.Text + " " + hesaplama.ToString("n2") + " TL Yapıyor";
+            }
+            else
+            {
+                MessageBox.Show("Kur Adet Boş Olamaz!", "Hata");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            cboKur.Items.Add("Dolar");
+            cboKur.Items.Add("Euro");
+            cboKur.Items.Add("Sterlin");
+            cboKur.SelectedIndex = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 5000;
+            DovizGoster();
+        }
+    }
+}
